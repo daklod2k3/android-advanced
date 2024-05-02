bfb5b3d4715795a8cf6aed6c692ec072392be7ac
Bùi Bích Nhung <blbnhung03@gmail.com>
2024-05-03 00:08:26 +0700

    Uppdate

--- app/src/main/java/com/daklod/techshop/MainActivity.java
+++ app/src/main/java/com/daklod/techshop/MainActivity.java
@@ -31,49 +31,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent main = getIntent();
-        if (main.getStringExtra("admin") != null) initUI();
-        else
-        try {
-            new CheckAuthTask().execute();
-        }catch (Exception e){
-            Log.e(TAG, "onCreate: ", e);
+        if (main.getStringExtra("admin") != null)
             initUI();
-        }
+        else
+            try {
+                new CheckAuthTask().execute();
+            } catch (Exception e) {
+                Log.e(TAG, "onCreate: ", e);
+                initUI();
+            }
 
     }
 
-    private void initUI(){
+    private void initUI() {
         ReplaceFragment(new HomeFragment());
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Log.d("fragment", String.valueOf(item.getItemId()));
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.home:
                     ReplaceFragment(new HomeFragment());
                     break;
                 case R.id.sample:
                     ReplaceFragment(new SampleFragment());
                     break;
+                case R.id.history:
+                    ReplaceFragment(new HistoryFragment());
+                    break;
             }
-            return  true;
+            return true;
         });
     }
 
-    private void ReplaceFragment(Fragment fragment){
+    private void ReplaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
 
-    class CheckAuthTask extends AsyncTask<Void, Void, Boolean>{
+    class CheckAuthTask extends AsyncTask<Void, Void, Boolean> {
 
         @Override
         protected Boolean doInBackground(Void... values) {
             try {
                 return new CheckAuthAPI().checkAuth();
-            }catch (Exception e){
+            } catch (Exception e) {
                 Log.d(TAG, "doInBackground: ");
                 publishProgress();
             }
@@ -95,9 +99,10 @@ public class MainActivity extends AppCompatActivity {
             if (aBoolean) {
                 initUI();
                 return;
-            };
-//            new Intent(getBaseContext(), LoginActivity.class);
-//            startActivity();
+            }
+            ;
+            // new Intent(getBaseContext(), LoginActivity.class);
+            // startActivity();
             CheckAuthAPI.clearToken(getBaseContext());
             alertLogout("Thông tin đăng nhập hết hạn", "Bạn sẽ trở về màn hình đăng nhập");
 
@@ -106,32 +111,31 @@ public class MainActivity extends AppCompatActivity {
 
     private void alertLogout(String title, String message) {
         AlertDialog alertDialog = new AlertDialog.Builder(this)
-        //set icon
-        .setIcon(android.R.drawable.ic_dialog_alert)
-        //set title
-        .setTitle(title)
-        //set message
-        .setMessage(message)
-        //set positive button
-        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //set what would happen when positive button is clicked
-                startActivity(new Intent(getBaseContext(), LoginActivity.class));
-                finish();
-            }
-        }).setCancelable(false).show();
-        //set negative button
-//        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
-//                //set what should happen when negative button is clicked
-//                Toast.makeText(getApplicationContext(),"Nothing Happened",Toast.LENGTH_LONG).show();
-//            }
-//        })
-//        .show();
+                // set icon
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                // set title
+                .setTitle(title)
+                // set message
+                .setMessage(message)
+                // set positive button
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // set what would happen when positive button is clicked
+                        startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                        finish();
+                    }
+                }).setCancelable(false).show();
+        // set negative button
+        // .setNegativeButton("No", new DialogInterface.OnClickListener() {
+        // @Override
+        // public void onClick(DialogInterface dialogInterface, int i) {
+        // //set what should happen when negative button is clicked
+        // Toast.makeText(getApplicationContext(),"Nothing
+        // Happened",Toast.LENGTH_LONG).show();
+        // }
+        // })
+        // .show();
     }
-
-
 
 }
