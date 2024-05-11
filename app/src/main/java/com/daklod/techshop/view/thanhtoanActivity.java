a1bf9f2a6a3109a04dfff86342311d1d22fcf52a
Man2k3 <Anmanproone94@gmail.com>
2024-05-11 16:24:44 +0700

    up giao dien user, giao dien thanh toan

---
+++ app/src/main/java/com/daklod/techshop/view/thanhtoanActivity.java
@@ -0,0 +1,16 @@
+package com.daklod.techshop.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.daklod.techshop.R;
+
+public class thanhtoanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanhtoan);
+    }
+}
